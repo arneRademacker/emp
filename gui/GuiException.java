0bf8aa319a1b2e3b58e36cc555c57ea0f0221558
tjey2400 <tim-julian.hass@web.de>
2013-12-05 11:59:18 +0100

    GUI ready

---
+++ gui/GuiException.java
@@ -0,0 +1,38 @@
+package gui;
+
+import javax.swing.JOptionPane;
+
+public class GuiException {
+
+	public static void letzterDS(){
+		JOptionPane.showMessageDialog(null,
+			    "Kein weiterer Datensatz vorhanden!",
+			    "Logischer Fehler",
+			    JOptionPane.ERROR_MESSAGE);
+	}
+	
+	public static void loeschen(){
+		if (JOptionPane.showConfirmDialog(null, "Wollen Sie den Datensatz wirklich löschen?", "Achtung!",
+		        JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+		    // yes option
+			
+		} else {
+		    // no option
+			
+		}
+	
+	}
+	public static void suchVorname(){
+		String suchVorname = JOptionPane.showInputDialog(null," Bitte geben Sie den Vornamen ein: ",
+				"Vornamensuche",
+				JOptionPane.PLAIN_MESSAGE);
+		System.out.println(suchVorname);
+	}
+	public static void suchNachname(){
+		String suchNachname = JOptionPane.showInputDialog(null,"Bitte geben Sie den Nachnamen ein: ",
+				"Nachnamensuche",
+				JOptionPane.PLAIN_MESSAGE);
+		System.out.println(suchNachname);
+	}
+
+}
