208a7dbfb89d91b928c306ccf2a4212d227dde90
Arne Rademacker <arne@enough.de>
2013-12-12 13:56:34 +0100

    cleans up the launcher.java, adds a line of code that sets the persistence type (e.g. sqlite, xml)

--- eclipse/src/de/szut/emp/Launcher.java
+++ eclipse/src/de/szut/emp/Launcher.java
@@ -1,20 +1,12 @@
 package de.szut.emp;
 
-import de.szut.emp.businessObjects.IEmailContact;
-import de.szut.emp.dataLayer.DataLayerManager;
-import de.szut.emp.dataLayer.businessObjects.EmailContact;
 import de.szut.emp.dataLayer.settings.SettingsManager;
 import de.szut.emp.gui.MainWindow;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-//		SettingsManager.getInstance().setPersistenceType(SettingsManager.PersistenceType.XML);
-//		IEmailContact emailContact = new EmailContact();
-//		emailContact.setVorname("John");
-//		emailContact.setNachname("Doe");
-//		emailContact.setEmail("John.Doe@random.com");
-//		DataLayerManager.getInstance().getDataLayer().getEmailContactDao().create(emailContact);
+		SettingsManager.getInstance().setPersistenceType(SettingsManager.PersistenceType.SQLITE);
 		(new MainWindow()).init();
 	}
 }
